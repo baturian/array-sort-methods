8a06e77207f799b7c7d1673f8a49655a2e10e1f5
Sergey <45436664+baturian@users.noreply.github.com>
2021-05-04 20:14:26 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@
 export const sortMethods = [
   { value: 'Selection sort', id: 'selection' },
   { value: 'Bubble sort', id: 'bubble' },
-  { value: 'Quick sort', id: 'quick' }
+  { value: 'Quick sort', id: 'quick' },
 ];
